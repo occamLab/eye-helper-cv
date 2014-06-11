6db2e87ab5847e10a3d2b890323788a877f03335
greenteawarrior <emily.wang@students.olin.edu>
2014-06-11 15:10:33 -0400

    Contour shenanigans with live video

--- contourTest.py
+++ contourTest.py
@@ -10,11 +10,16 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-im = cap.read()[1]
+while(True):
+    im = cap.read()[1]
+    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(imgray,127,255,0)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(im, contours, -1, (0,255,0), 3)
+    cv2.imshow("window title", im)
+    if cv2.waitKey(1) & 0xFF == ord('q'): #hit escape or the q key to end the loop
+        break
 
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(im, contours, -1, (0,255,0), 3)
-cv2.imshow("window title", im)
-cv2.waitKey()
+#when everything's done, release the capture
+cap.release()
+cv2.destroyAllWindows()
