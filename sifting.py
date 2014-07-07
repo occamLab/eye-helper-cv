c0af9cbf65e667c9ecf451cca5f673321f139a30
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-07-07 10:31:50 -0400

    sift matching with visualization, still does not have single object isolation, but does show keypoint matching

--- sifting.py
+++ sifting.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy as sp
 
 img1 = cv2.imread('cookie-query.jpg',0) # queryImage
 img2 = cv2.imread('cookie-train.bmp',0) # trainImage
@@ -9,23 +10,40 @@ img2 = cv2.imread('cookie-train.bmp',0) # trainImage
 sift = cv2.SIFT()
 
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+k1, d1 = sift.detectAndCompute(img1,None)
+k2, d2 = sift.detectAndCompute(img2,None)
 
 # BFMatcher with default params
 bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+matches = bf.knnMatch(d1,d2, k=2)
 
 # Apply ratio test
 good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
-        good.append([m])
+        good.append(m)
 
+
+
+h1, w1 = img1.shape[:2]
+h2, w2 = img2.shape[:2]
+view = sp.zeros((max(h1, h2), w1 + w2, 3), sp.uint8)
+view[:h1, :w1, 0] = img1
+view[:h2, w1:, 0] = img2
+view[:, :, 1] = view[:, :, 0]
+view[:, :, 2] = view[:, :, 0]
+
+for m in good:
+    # draw the keypoints
+    # print m.queryIdx, m.trainIdx, m.distance
+    color = tuple([sp.random.randint(0, 255) for _ in xrange(3)])
+    cv2.line(view, (int(k1[m.queryIdx].pt[0]), int(k1[m.queryIdx].pt[1])) , (int(k2[m.trainIdx].pt[0] + w1), int(k2[m.trainIdx].pt[1])), color)
+
+
+cv2.imshow("view", view)
+cv2.waitKey(0)
 # cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,flags=2)
 
-plt.imshow(img3),plt.show()
 
 # img = cv2.imread('cookie_00270.jpg')
 # gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
