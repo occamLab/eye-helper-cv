9a7126b035741f391b71954f35101c2384ade42c
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-07-11 15:02:01 -0400

    modified plotdata and moved pickles to new home

--- plotdata.py
+++ plotdata.py
@@ -5,9 +5,10 @@ import pickle
 #graphs we're interested in:
 # speed v. accuracy
 #   rotation, affine, overall
+#temporal distance from training image
 
 
-def plot(data, plot_type, plot_xlabel, plot_ylabel, plot_title):
+def plot(data, plot_xlabel, plot_ylabel, plot_title):
     """Inputs:
             data -> in a list [x, y]
             plot type -> string either 'scatter' or 'histogram'
@@ -25,7 +26,11 @@ def plot(data, plot_type, plot_xlabel, plot_ylabel, plot_title):
     plt.show()    
 
 if __name__ == '__main__':
-    temp = open('./OT-res/pickles/p0/catfood.p', 'r')
+    temp = open('./OT-res/pickles/p0/cereal.p', 'r')
     data = pickle.load(temp)
     print data
-    #plot(data = , plot_type = , plot_xlabel = , plot_ylabel = , plot_title = )
+    for key in data:
+        x = data[key][3]
+        y = data[key][2]
+        plt.hold(True) 
+        plot(data = [x,y], plot_xlabel = 'time of run (s)', plot_ylabel = 'Percent accuracy' , plot_title = 'Time (s) v. accuracy ' )
