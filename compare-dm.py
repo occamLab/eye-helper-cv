a39d42ebc6660ca9b70ed24613099035128db299
greenteawarrior <emily.wang@students.olin.edu>
2014-07-11 17:17:57 -0400

    We should be done with the assertion error things now...

--- compare-dm.py
+++ compare-dm.py
@@ -81,6 +81,7 @@ def compare_dm(videoname, trainimg, gt_csv, visualize = False):
 
     # Loop through the methods, store in a dictionary somehow
     for method in d_methods:
+
         print "starting %s" % method
         #Initializing things for running the loop on this method
         method_start_time = time.time()
@@ -92,6 +93,7 @@ def compare_dm(videoname, trainimg, gt_csv, visualize = False):
         correctmatches = 0
         totalmatches = 0
         good_matches = []
+        frame_total_times = []
 
         # Opening the ground truth csv
         csvfile = open('./gstore-csv/%s' % gt_csv, 'rb')            
@@ -104,6 +106,7 @@ def compare_dm(videoname, trainimg, gt_csv, visualize = False):
 
         # Open the training image 
         t = cv2.imread(trainimg,0)
+        print 'trainimg is %s' % trainimg
 
         # Get the training image's descriptors and keypoints
         t_k = detector.detect(t)
@@ -185,6 +188,7 @@ def compare_dm(videoname, trainimg, gt_csv, visualize = False):
                 frametimes['vis_time'].append(0)
 
             frame_end_time = time.time()
+            frame_total_times.append(frame_end_time - frame_start_time)
             frametimes['start_to_end_time'].append(frame_end_time - frame_start_time)
 
         method_end_time = time.time()
@@ -198,7 +202,9 @@ def compare_dm(videoname, trainimg, gt_csv, visualize = False):
             frametimes[key] = sum(value) / float(len(value))
 
         # Compute success ratios for all the rows for this particular method
-        successes[method] = [correctmatches, totalmatches, float(correctmatches)/float(totalmatches)*100, method_total_time, frametimes]
+        # successes[method] = [correctmatches, totalmatches, float(correctmatches)/float(totalmatches)*100, method_total_time, frametimes]
+        successes[method] = [float(correctmatches)/float(totalmatches)*100, frame_total_times]
+
     # Return dictionary of method: success ratio
     return successes
 
@@ -207,16 +213,17 @@ def print_dm_res(d, csv_str):
     for key in d:
         print "%s with the %s snippet" % (key, csv_str)
         current = d[key]
-        print "\tcorrectmatches: %d" % current[0]
-        print "\ttotalmatches: %d" % current[1]
-        print "\t%d%% accuracy" % current[2] 
-        print "\tmethod total runtime: %f seconds" % current[3]
-        frametimes = current[4]
-        print "\t\taverage start to end time per frame: %f seconds" % frametimes['start_to_end_time']
-        print "\t\taverage image opening time per frame: %f seconds" % frametimes['open_time']
-        print "\t\taverage keypoint detection time per frame: %f seconds" % frametimes['kp_detect_time']
-        print "\t\taverage feature matching time per frame: %f seconds" % frametimes['match_time']
-        print "\t\taverage visualization time per frame: %f seconds" % frametimes['vis_time']
+        # print "\tcorrectmatches: %d" % current[]
+        # print "\ttotalmatches: %d" % current[]
+        print "\t%d%% accuracy" % current[0] 
+        # print "\tmethod total runtime: %f seconds" % current[]
+        print current[1]
+        # frametimes = current[]
+        # print "\t\taverage start to end time per frame: %f seconds" % frametimes['start_to_end_time']
+        # print "\t\taverage image opening time per frame: %f seconds" % frametimes['open_time']
+        # print "\t\taverage keypoint detection time per frame: %f seconds" % frametimes['kp_detect_time']
+        # print "\t\taverage feature matching time per frame: %f seconds" % frametimes['match_time']
+        # print "\t\taverage visualization time per frame: %f seconds" % frametimes['vis_time']
         print '\n'
 
 if __name__ == '__main__':
