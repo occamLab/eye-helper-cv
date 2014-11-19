cabc5679828f1ed0371196d405523ddd8232814e
greenteawarrior <emily.wang@students.olin.edu>
2014-11-19 14:32:22 -0500

    eye_helper ROS package

---
+++ eye_helper/scripts/compare_kpd.py
@@ -0,0 +1,299 @@
+import cv2
+import numpy as np 
+import csv
+import pickle
+import scipy.ndimage 
+import scipy.stats
+import os
+import math
+import matplotlib.pyplot as plt  
+import pprint as pp
+
+from object_match import mean_shift
+from plot_data import plot
+
+def calc_center(g_truth):
+    """ 
+        Function finds the center of object box
+
+        Inputs: g_truth -> four corners of a box
+        Returns: center -> the center of the box
+    """
+
+    x = int((g_truth[2]+g_truth[0])/2)
+    y = int((g_truth[3]+g_truth[1])/2)
+    return x, y
+
+def superdata(q_pickle, t_pickle, q_gtruth, t_gtruth, frame, method, t_img):
+    """
+        we may or may not rename this function in the near future to be something more descriptive... 
+        --emily and lindsey, july 18, 2014
+
+        Function calculates all the data we think we might later use
+        Inputs:
+            q_pickle -> keypoint and descriptor data from query image
+            t_pickle -> keypoint and descriptor data from training image
+            g_truth -> coordinates of labeled box around actual object
+            frame -> frame number
+            method -> kp detector method used to get points
+            t_img -> training image frame number
+
+        We assume that the object has been perfectly selected so we use ground truth values for the training image region.
+
+        Returns:
+            c_center -> center of the object tracking circle
+            b_center -> center of the object
+            match -> boolean True or False
+            frame -> the number of the frame
+            t_frame -> number of the training image
+            kp_matches -> total number of matched keypoints
+            c_matches -> number of correctly matched keypoints
+    """
+    #load query image data
+    q = pickle.load(open(q_pickle, 'rb'))
+    q_k = q[0]
+    q_d = q[1]
+
+    #load training image data
+    t = pickle.load(open(t_pickle, 'rb'))
+    t_k = t[0]
+    t_d = t[1]
+
+    #iterate through the training keypoints and only keep those that are within the selected object
+    #In this case we are using the training image's ground truth as the selection
+    train_d = []
+    train_k =[]
+    for index in range(len(t_k)):
+        if t_gtruth[2]<=t_k[index][0]<=t_gtruth[0] and t_gtruth[3]<=t_k[index][1]<=t_gtruth[1]:
+            train_d.append(t_d[index])
+            train_k.append(t_k[index])
+    t_k = train_k
+    t_d = np.array(train_d)
+
+    #Selects the correct matcher parameters depending on the keypoint detector used
+    if method == 'SIFT' or method == 'SURF':
+        bf = cv2.BFMatcher()
+    else: #for ORB or BRISK
+        bf = cv2.BFMatcher(normType = cv2.NORM_HAMMING)
+
+    try:
+        #matches keypoints of *training* image to query, not other way around
+        matches = bf.knnMatch(t_d, q_d, k=2)
+
+        good_matches = []
+        #goes through and only keeps matches passing a nearest neighbor test to reduce false matches
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                # Get coordinate of the match
+                m_x = int(q_k[m.trainIdx][0])
+                m_y = int(q_k[m.trainIdx][1])
+                good_matches.append((m_x, m_y))
+
+        #uses mean_shift to find the object from the keypoints detected
+        c_center, radius = mean_shift(hypothesis = (100, 100), keypoints = good_matches, threshold = 10, frame = frame)
+        b_center = calc_center(q_gtruth)
+        
+        #checking each keypoint in good_matches
+        kp_matches = len(good_matches)
+        c_matches = 0
+        for match in good_matches:
+            if q_gtruth[2]<=match[0]<=q_gtruth[0] and q_gtruth[3]<=match[1]<=q_gtruth[1]:
+                c_matches +=1
+    
+        #checking if the center is within the labeled query box
+        if q_gtruth[2]<=c_center[0]<=q_gtruth[0] and q_gtruth[3]<=c_center[1]<=q_gtruth[1]:
+            match = True
+        else:
+            match = False
+
+        # distance from center 
+        d_from_c = math.hypot(c_center[0]-b_center[0], c_center[1]-b_center[1])
+        # return c_center, b_center, d_from_c, match, frame, t_img, kp_matches, c_matches
+        return {'c_center': c_center, 'b_center': b_center, 'd_from_c': d_from_c,'match': match, 'frame': frame, 't_img': t_img, 'kp_matches': kp_matches, 'c_matches': c_matches}
+   
+    except:
+        print "Likely there are no good matches..."
+        return None
+
+
+def plot_superdata(dstr, methods, framerange):
+
+    # open data to plot and put it into a dictionary :)
+    data = {}
+    correct_centers = {}
+    for trial in range(framerange[0], framerange[1], 20):
+        data[trial] ={}
+        for mstr in methods:
+            data[trial][mstr]= pickle.load(open('../OT-res/compare_kpd_plots/%s_%s.p' % (dstr, mstr), 'rb'))[trial]
+            correct_centers[mstr] = []
+    n = 2                    
+    trial_order = []
+    for trial in data:
+        for method in data[trial]:
+            try:
+                trialdata = data[trial][method]
+                # print(trialdata)
+                frames = [int(x) - trial for x in trialdata['frame numbers']]
+                d_from_c = [trialdata['distance from center'][x]/trialdata['hypotenuse'][x] for x in range(len(trialdata['distance from center']))]
+                d_from_c =scipy.ndimage.filters.gaussian_filter1d(np.array(d_from_c), 2) #smoothed data to facilitate visualization/analysis; we chose a sigma of 2 after trying some values and staring at the plots 
+                #variable is actually the *precent* of correctly matched centers not the number   
+                correct_centers[method].append([trial, float(len([x for x in trialdata['c_match'] if x is True]))/len(frames)*100])
+                # #plotting the normalized distance from center comparison between different keyopint detector methods graph
+                plt.figure(n, figsize=(15,10))
+                plt.plot(frames, d_from_c, '-', label = method)
+                plt.xlabel('# of frames since training image (frames)')
+                plt.ylabel(('Distance between guessed and actual object center (hypotenuse lengths)'))
+                plt.title('Distance from actual center v. Frames since training image for cookie trial #%d Gaussian filter sigma = 2' %trial)
+                plt.legend()
+            except:
+                print 'yo'
+                continue
+        n += 1
+        plt.savefig("../OT-res/compare_kpd_plots/method_comparison_plots/%s_trial%d_dfc_plot_sigma_two.png" % (dstr, trial))
+
+def gen_plottables(methods, dataset, framerange):
+    #plot-friendly data structure
+    #{key = training image number : value = {frame numbers: [], boxes: {}, overall accuracy: [], :distance from center: [], total kp matches: [], correct kp matches: []}}
+    plottables = {}
+
+    # cookie sift (for the entire cookie video)
+    framestart = framerange[0]
+    framemax = framerange[1]
+    
+    for m in methods:
+        t_img_number = framestart 
+        while t_img_number < framemax:
+            print m
+            print t_img_number
+            #instantiating plottables for this training image trial
+            plottables[t_img_number] = {'frame numbers': [],
+                                        'boxes': {}, 
+                                        'hypotenuse': [],
+                                        'c_match': [], 
+                                        'overall accuracy': 0, 
+                                        'distance from center': [], 
+                                        'total kp matches': [], 
+                                        'correct kp matches': []}
+
+            ################ start of a t_img trial
+            for line in reversed(open('../gstore-csv/%s.csv' % dataset).readlines()):
+                row = line.rstrip().split(',')
+                plottables[t_img_number]['boxes'][int(row[0])] = [int(row[1]), int(row[2]), int(row[3]), int(row[4])]
+
+                #starting with training image number, calculate the superdata.
+
+                #training image is never a "future frame"
+                #for example, if the training image is frame 144, the test starts from frame 144, not frame 124
+
+                if int(row[0]) == t_img_number:
+                    q_img_number = int(row[0])
+                    plottables[t_img_number]['frame numbers'].append(row[0])
+                    t_gtruth = [int(row[1]), int(row[2]), int(row[3]), int(row[4])]
+                    q_gtruth = [int(row[1]), int(row[2]), int(row[3]), int(row[4])]
+                    data = superdata(q_pickle = '../OT-res/kp_pickles/%s/%s/%s_00%d_keypoints.p' % (dataset, m, dataset, q_img_number), 
+                        t_pickle = '../OT-res/kp_pickles/%s/%s/%s_00%d_keypoints.p' % (dataset, m, dataset, t_img_number), 
+                        q_gtruth = q_gtruth,
+                        t_gtruth = t_gtruth, 
+                        frame = q_img_number, 
+                        method = m, 
+                        t_img = t_img_number)    
+
+                    #saving the results
+                    if data != None:        
+                        hypotenuse = math.sqrt((q_gtruth[0]-q_gtruth[2])**2 + (q_gtruth[1]-q_gtruth[3])**2)
+                        plottables[t_img_number]['hypotenuse'].append(hypotenuse)
+                        plottables[t_img_number]['c_match'].append(data['match'])
+                        plottables[t_img_number]['distance from center'].append(data['d_from_c'])
+                        plottables[t_img_number]['total kp matches'].append(data['kp_matches'])
+                        plottables[t_img_number]['correct kp matches'].append(data['c_matches'])
+
+                elif int(row[0]) > t_img_number:
+                    q_img_number = int(row[0])
+                    q_gtruth = [int(row[1]), int(row[2]), int(row[3]), int(row[4])]
+                    data = superdata(q_pickle = '../OT-res/kp_pickles/%s/%s/%s_00%d_keypoints.p' % (dataset, m, dataset, q_img_number), 
+                                    t_pickle = '../OT-res/kp_pickles/%s/%s/%s_00%d_keypoints.p' % (dataset, m, dataset, t_img_number), 
+                                    q_gtruth = q_gtruth,
+                                    t_gtruth = t_gtruth, 
+                                    frame = q_img_number, 
+                                    method = m, 
+                                    t_img = t_img_number)
+
+                    #saving the results
+                    if data != None:
+                        hypotenuse = math.sqrt((q_gtruth[0]-q_gtruth[2])**2 + (q_gtruth[1]-q_gtruth[3])**2)
+                        plottables[t_img_number]['hypotenuse'].append(hypotenuse)
+                        plottables[t_img_number]['c_match'].append(data['match'])
+                        plottables[t_img_number]['frame numbers'].append(row[0])
+                        plottables[t_img_number]['distance from center'].append(data['d_from_c'])
+                        plottables[t_img_number]['total kp matches'].append(data['kp_matches'])
+                        plottables[t_img_number]['correct kp matches'].append(data['c_matches'])
+
+            try:
+                plottables[t_img_number]['overall accuracy'] = ( float(sum(plottables[t_img_number]['correct kp matches'])) / sum(plottables[t_img_number]['total kp matches']) ) * 100
+            except:
+                plottables[t_img_number]['overall accuracy'] = 0
+            ################ end of a t_img trial
+            t_img_number += 20 #try a different training image (every 20 frames)... from frame 124 to 288 for cookie
+        pickle.dump(plottables, open('../OT-res/compare_kpd_plots/%s_%s.p' % (dataset, m), 'wb'))            
+
+def superANOVA(dstr, methods, framerange, accuracy = False, distance = False):
+    """
+    Update: We currently don't have enough data for an ANOVA test to be helpful (or it'd be violating assumptions of these types of F/p/t tests anyways...)
+    Will potentially revisit when we have more data and stats would be useful. --Emily and Lindsey, July 25, 2014
+
+    A function to determine if there is any actual statistically siginificant differences in or data
+    between the different keypoint detection methods.
+
+    """
+    data = {}
+    correct_centers = {}
+    testables = {}
+    test_res = {}
+
+    #Loads the pickles which have all the possible data that we could want
+    for trial in range(framerange[0], framerange[1], 20):
+        data[trial] ={}
+        for mstr in methods:
+            data[trial][mstr]= pickle.load(open('../OT-res/compare_kpd_plots/%s_%s.p' % (dstr, mstr), 'rb'))[trial]
+            correct_centers[mstr] = []
+            testables[mstr] = []
+
+    # generates the accuracy and normalized distance from center information that we are interested in 
+    for trial in data:
+        d_from_c = {}
+        for method in data[trial]:
+            try:
+                trialdata = data[trial][method]
+                frames = [int(x) - trial for x in trialdata['frame numbers']]
+                d_from_c[method] = ([trialdata['distance from center'][x]/trialdata['hypotenuse'][x] for x in range(len(trialdata['distance from center']))])
+                #variable is actually the *percent* of correctly matched centers not the number   
+
+                if accuracy:
+                    testables[method].append(len([x for x in trialdata['c_match'] if x is True]) / len(frames) * 100)
+            except:
+                print("If only I hadn't failed!")
+
+        #If we are using this to compare distances use the relevant data generated above, do this for every trial
+        if distance:
+            f, p = scipy.stats.f_oneway(*[d_from_c[methods] for methods in d_from_c]) 
+            test_res[trial] = p 
+
+    # If we are interested in comparing accuracy between method use relevant data from above, do this only once
+    if accuracy:
+        f,p = scipy.stats.f_oneway(*[testables[methods] for methods in testables])
+        test_res = p
+        
+    return test_res
+
+if __name__ == '__main__':
+
+    #loops for datasets, methods, t_img while, q_imgs
+
+    methods = ['SURF', 'SIFT', 'BRISK', 'ORB']
+    # plottables = gen_plottables(methods, 'cereal', [512, 695]) 
+    dstr = 'cookie'
+    framerange = [124, 288]
+
+    print superANOVA(dstr, methods, framerange, distance = True)
+    # plot_superdata(dstr, methods, framerange)
+
---
+++ eye_helper/scripts/object_match.py
@@ -0,0 +1,245 @@
+import cv2
+import numpy as np 
+import scipy as sp 
+import csv
+import pandas
+
+"""
+Object matching shenanigans with meanshift. 
+
+--Emily and Lindsey, July 25, 2014
+"""
+
+def draw_circles(img, c, radius, kp=None):
+    """Takes in:
+        img -> opened numpy array of image
+        kp -> list of the keypoint coordinates 
+        c -> center of 'mass' of keypoints
+        radius -> estimated radius of object
+    """
+    if kp != None:
+        for k in kp:
+            cv2.circle(img, (k[0], k[1]), 1, [255, 0, 0], 2)
+    # cv2.circle(img, c, radius, [100,255,0], 2)
+    if c != None:
+        cv2.circle(img, c, 6, [0,0,255], 6)
+    # return img
+
+def match_object(previous, current, train_img, pos, frame=0, show = False, live = False, t = ()):
+    """
+    Takes in:
+        previous -> center of the previous image
+        current ->  image being analyzed (if not live: the img path)
+        train_img -> training image for keypoint matching
+        pos -> corners (left top, right bottom) of object in training img
+        show -> determines if visualization is displayed
+        t -> passes in keypoints and descriptors of training image IF we're using the live_meanshift.py file
+    Output:
+        new_center -> new center determined by meanshift
+                      if there are no matches, original center is returned instead
+
+    """
+
+    # corners of selected item in training image, accounts for the flipping
+    x1 = pos[2]
+    y1 = pos[3]
+    x2 = pos[0]
+    y2 = pos[1]
+
+    #I choose YOU! SIFT-emon
+    detector = cv2.SIFT()
+
+    if not live:
+        #Take in training image with coordinates of tracked object
+        t_img = cv2.imread(train_img)
+
+        #query image
+        q_img = cv2.imread(current)
+
+        #Keeps only keypoints within the grocery item selection
+        t_k, t_d = detector.detectAndCompute(t_img, None)   #training image
+
+        train_d = []
+        train_k = []
+
+        for index in range(len(t_k)):
+            if x1<=t_k[index].pt[0]<= x2 and y1<= t_k[index].pt[1] <=y2:
+                train_k.append(t_k[index])
+                train_d.append(t_d[index])
+        t_k = train_k
+        t_d = train_d
+
+
+    else:
+        #to account for how the webcam version of this deals with inputs differently
+        # t_img = train_img #should be cap.read()[1] 
+        q_img = current
+        t_k = t[0] #keypoints = locations (think x, y coords)
+        t_d = t[1] #descriptors = 128-element vectors (via SIFT) that gives information 
+                   #about gradient of surrounding change (changes in intensity) and orientation
+
+        train_d = []
+        train_k = []
+
+        for index in range(len(t_k)):
+            if x1<=t_k[index][0]<= x2 and y1<= t_k[index][1] <=y2:
+                train_k.append(t_k[index])
+                train_d.append(t_d[index])
+        t_k = train_k
+        t_d = train_d
+
+    #finds all the keypoints in the query image
+    q_k, q_d = detector.detectAndCompute(q_img, None)   #query image
+
+    try:
+        matcher = cv2.BFMatcher() #(normType = cv2.NORM_HAMMING)
+
+        #match list of object keypoints to query image, not other way around
+        matches = matcher.knnMatch(np.array(t_d), q_d, k =2)
+
+        #Nearest neighbor test to reduce false matches
+        good_matches = []
+        # b/c k=2....
+        # m is the nearest match
+        # n is the next nearest match
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                # Get coordinate of the match
+                m_x = int(q_k[m.trainIdx].pt[0])
+                m_y = int(q_k[m.trainIdx].pt[1])
+                good_matches.append((m_x, m_y))
+
+        print 'length of good_matches %d' % len(good_matches)
+
+        new_center, current = mean_shift(hypothesis = (previous), 
+                                                    keypoints = good_matches, 
+                                                    threshold = 10, 
+                                                    current = q_img,
+                                                    show = show, 
+                                                    frame = frame, 
+                                                    live = True, 
+                                                    show_iterations = False)
+        return new_center, current
+
+    except Exception as inst: #printing the error associated with why the except code chunk ran
+        print inst
+        print "Likely there are no matches"
+        return None, current
+
+def mean_shift(hypothesis, keypoints, threshold, frame, current = None, show = False, live = False, show_iterations = False):
+    """
+    Inputs:
+        hypothesis -> Previous center point as a starting hypothesis
+        keypoints -> List of keypoint (x,y) coordinates
+        Threshold -> maximum acceptable difference in center between iterations (eg 10 pixels, 5 pixels)
+        current -> np array representing the image (for visualization)
+        show -> determines whether visualization is shown
+
+    Returns:
+        New center of keypoints
+        Radius
+        If show is true -> displays the center, keypoints and a circle around the object
+    """
+
+    n=0
+    if len(keypoints) > 1:
+
+        #assigns a value to the weighting constant -> based on 
+        #experimental results on cropped cookie_00274
+        c = 0.0001
+
+        #arbitrarily set diff high to go through loop at least once
+        diff = 1000
+
+        while(diff > threshold):
+            #sets up lists of weights and weights*position
+            x_weights = []
+            y_weights = []
+            weighted_x = []
+            weighted_y = []
+            #Creats a list of weighted points, where points near the 
+            #hypothesis have a larger weight
+            last_guess = hypothesis
+            for kp in keypoints:
+                x_val = np.exp(-c * (kp[0] - last_guess[0])**2)
+                x_weights.append(x_val)
+                weighted_x.append(x_val*kp[0])
+                y_val = np.exp(-c * (kp[1] - last_guess[1])**2)
+                y_weights.append(y_val)
+                weighted_y.append(y_val*kp[1])
+
+            #finds 'center of mass' of the points to determine new center
+            x = int(sum(weighted_x)/sum(x_weights))
+            y = int(sum(weighted_y)/sum(y_weights))
+
+            #update hypothesis
+            hypothesis = (x,y)
+
+            #difference between the current and last guess
+            diff = np.sqrt((last_guess[0] - x)**2 + (last_guess[1] - y)**2)
+
+
+            #visualizes moving center and displays keypoints for every meanshift iteration if show_iterations==True
+            if show_iterations:
+                img = np.copy(current)   #Needs to be np array (already opened by cv2)
+                for k in keypoints:
+                    cv2.circle(img, k, 2, [255, 0, 0], 2)
+                cv2.circle(img, hypothesis, 3, [0, 0, 255], 3)
+                cv2.circle(img, hypothesis, 5, [100,255,0], 2)
+                cv2.imshow('Frame %d: Current hypothesis, meanshift guess %d' % (frame, n), img)
+                # cv2.imwrite('../OT_res/meanshift_presentation/cookie_f%d_guess%d.jpg' % (frame, n), img)
+                cv2.waitKey(0)
+                n+=1
+        
+        #visualizes moving center and displays keypoints if show==True for the frame 
+        #(so, this happens once per call of the function)
+        if show:
+            draw_circles(img=current, kp=keypoints, c = hypothesis, radius = 5)
+            if not live:
+                cv2.imshow('current', current)
+                cv2.waitKey(0)
+
+        return hypothesis, current
+
+def dictionify(array):
+    """takes in an array and outputs a dictionary where the keys are the first value of each row"""
+    dictionary = {}
+    for row in array:
+        dictionary[row[0]] = row[1:]
+    return dictionary
+
+if __name__ == '__main__':
+    # initial values for prototyping w/ the cookies. :P
+    old_center = [1000, 170]
+    center = old_center
+    pos = [744,514,606,392]
+
+    # with open('../gstore_csv/cookie.csv', 'r') as csvfile:
+    #     reader = csv.reader(csvfile)
+
+    data = np.loadtxt('../gstore_csv/cookie.csv', dtype=int, delimiter=',')
+    data_dict = dictionify(data)
+
+    it_works = 0
+
+    start = 180
+    stop = 190
+    for frame in range(start, stop): 
+        cv2.destroyAllWindows()
+        print "Frame number: %d" % frame
+        center, current = match_object(previous = center, 
+                              current = '../gstore_snippets/cookie_snippet/cookie_00%d.jpg' % frame, 
+                              train_img = '../gstore_snippets/cookie_snippet/cookie_00177.jpg',
+                              pos = pos,
+                              show = False,
+                              frame = frame)
+        old_center = center
+        if data_dict[frame][2] <= old_center[0] <= data_dict[frame][0] and data_dict[frame][3] <= old_center[1] <= data_dict[frame][1]:
+            it_works = it_works +1
+        
+    percent_success = float(it_works)/(stop-start)
+    print(percent_success)
+
+
+
+  
