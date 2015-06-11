7f7f38947869ff656deea7bc3e030ffcea150a18
pdemetci <pinardemetci@gmail.com>
2015-06-11 15:56:40 -0400

    Updated code with f-r distance and sound info publisher

--- eye_helper/scripts/tango_pose_calc2.py
+++ eye_helper/scripts/tango_pose_calc2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-
 import cv2
 import numpy as np
 import rospy
 import math
 import subprocess
 import rospkg
+import collections
+
 # from cv_bridge import CvBridge, CvBridgeError
 # from image_selector import *
 # from object_matcher import *
@@ -31,6 +32,9 @@ class TangoPoseCalc():
         self.forward_distance_pub = rospy.Publisher('/forward_distance', Float64, queue_size=10)
         self.right_distance_pub = rospy.Publisher('/right_distance', Float64, queue_size=10)
 
+        # New topics for Tango to publish sound info to:
+        self.sound_pub=rospy.Publisher('/sound_info', String,queue_size=10)
+
         self.rospack = rospkg.RosPack();
         self.path = self.rospack.get_path('eye_helper') + '/../GeneratedSoundFiles/'
         # Stuff for Tango coord use:
@@ -39,7 +43,18 @@ class TangoPoseCalc():
         self.playback_interval = rospy.Duration(0.2)
         self.last_tone = rospy.Time.now()
 
-    def play_wave(self, volume, mix=None):
+        # All initialized as None but defined in play_audio():
+        self.sound_info = None
+
+    def sound_publish(self):
+        rospy.init_node('sound_played', anonymous=True)
+        rate = rospy.Rate(self.playback_interval)
+        while not rospy.is_shutdown():
+            rospy.loginfo(self.sound_info)
+            self.sound_pub.publish(self.sound_info)
+            rate.sleep()
+
+    def play_wave(self, volume, mix):
         """
         plays an inputted wav file
         """
@@ -63,23 +78,6 @@ class TangoPoseCalc():
         if d != None:
             self.playback_interval = rospy.Duration(d/2.0)
 
-
-    def get_h_to_play(self, rz):
-        """
-        returns the h-value to play. Currently the numbers are more-or-less arbitrary; TODO: test/figure out what works well. rz = relative_z of the target compared to the tango.
-        """
-        if rz <= -0.6:
-            return 1
-        elif rz <= -0.2:
-            return 2
-        elif rz <= 0.2:
-            return 3
-        elif rz <=0.6:
-            return 4
-        else:
-            return 6
-
-
     def play_audio(self):
         """
             Uses class variables; plays the corresponding sound file.
@@ -101,83 +99,58 @@ class TangoPoseCalc():
 
         if d == None:
             return
-        # elif d >= 1:
-
-
-        #     if self.target_z != None: # This check is probably now unnecessary.
-        #         self.update_angle_to_go()
-        #         self.angle_to_play = int(5 * round(float(self.angle_to_go)/5))
-        #         angle_to_volume = {0: 21, 5: 22, 10: 23, 15: 24, 20: 25, 25: 26, 30: 27, 35: 28, 40: 29, 45: 30, 50: 31, 55:31, 60:31, 65:31, 70:31, 75 : 31, 80 : 31, 85 : 31, 90: 31}
-        #         desired_volume = angle_to_volume[abs(self.angle_to_play)]
-        #         # print self.angle_to_play
-        #         if self.angle_to_play >= 0:
-        #             self.filename = "{}height{}angle{}.wav".format(self.path, 4, 90)
-        #             #self.filename = "{}height{}angle{}.wav".format(self.path, 4, self.angle_to_play)
-        #         else:
-        #             self.filename = "{}height{}angle_{}.wav".format(self.path, 4, 90)
-        #             #                self.filename = "{}height{}angle_{}.wav".format(self.path, 4, abs(self.angle_to_play))
-        #   
+            
         else:
             #relative_z = self.target_z - self.z
 
             ###Code for left-right mix:
 
-            if self.tracker.right_distance < 0:
+            if self.tracker.right_distance < -0.03:
                 desired_mix = ['100','0']
-            elif self.tracker.right_distance==0:
+            elif self.tracker.right_distance<0.03 or self.tracker.right_distance> -0.03 :
                 desired_mix = ['50','50']
-            else:
+            else: 
                 desired_mix = ['0','100']
 
             ### Code for playing volume based on y-distance (instead of yaw)### 
             relative_y = abs(self.tracker.right_distance)
-            if relative_y > 1.8:
-                relative_y=2
-            if relative_y > 1.6 and relative_y <=1.8:
-                relative_y=1.8
-            if relative_y >1.4 and relative_y <=1.6 :
-                relative_y=1.6
-            if relative_y > 1.2 and relative_y <=1.4:
-                relative_y=1.4
-            if relative_y > 1.0 and relative_y <=1.2:
-                relative_y=1.2
-            if relative_y > 0.8 and relative_y <=1.0:
+            if relative_y == 1.2 or relative_y > 1.2:
+                relative_y = 1.2
+            elif relative_y >= 1.1 or relative_y < 1.2: 
+                relative_y=1.1
+            elif relative_y >= 1.0 or relative_y < 1.1: 
                 relative_y=1.0
-            if relative_y > 0.6 and relative_y <=0.8:
+            elif relative_y >= 0.9 or relative_y < 1.0: 
+                relative_y=0.9
+            elif relative_y >= 0.8 or relative_y < 0.9: 
                 relative_y=0.8
-            if relative_y > 0.4 and relative_y <=0.6:
+            elif relative_y >= 0.7 or relative_y < 0.8: 
+                relative_y=0.7
+            elif relative_y >= 0.6 or relative_y < 0.7: 
                 relative_y=0.6
-            if relative_y > 0.2 and relative_y <=0.4:
+            elif relative_y >= 0.5 or relative_y < 0.6: 
+                relative_y=0.5
+            elif relative_y >= 0.4 or relative_y < 0.5: 
                 relative_y=0.4
-            if relative_y<=0.2:
+            elif relative_y >= 0.3 or relative_y < 0.4: 
+                relative_y=0.3
+            elif relative_y >= 0.2 or relative_y < 0.3: 
                 relative_y=0.2
-
-            possible_y=[0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
-            possible_volume=[x for x in range(20,66) if x%5 == 0]   #Just because I did not have any audio files where volume>31. Can be commented out/deleted once we create such files 
+            else:
+                relative_y=0.0
+            possible_y=[0.0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]
+            possible_volume=[20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75]   #Just because I did not have any audio files where volume>31. Can be commented out/deleted once we create such files 
             y_to_volume={}
             for i in range(len(possible_y)):
                 y_to_volume[possible_y[i]]=possible_volume[i]
-
             desired_volume=y_to_volume[relative_y]
 
-            self.filename="{}volume{}.wav".format(self.path, 22)
-            desired_volume*=4
+            filename="{}height4angle15.wav".format(self.path)
+            desired_volume*=4 % rospy.get_time()
             print "desired volume", desired_volume
             self.play_wave(desired_volume, desired_mix)
-
-            ### Code for angle_to_volume ###
-
-            # angle_to_volume = {0: 21, 5: 22, 10: 23, 15: 24, 20: 25, 25: 26, 30: 27, 35: 28, 40: 29, 45: 30, 50: 31, 55:31, 60:31, 65:31, 70:31, 75 : 31, 80 : 31, 85 : 31, 90: 31}
-            # desired_volume = angle_to_volume[abs(self.angle_to_play)]
-            # h_to_play = self.get_h_to_play(relative_z)
-
-            #     #                self.filename = "{}height{}angle_{}.wav".format(self.path, 4, abs(self.angle_to_play))
-            # print "volume is", desired_volume
-            # print "desired mix", desired_mix
-            # #desired_mix = None
+            self.sound_info= 'mix: ' + str(desired_mix) + ' volume: ' + str(desired_volume) + " sound path: " +str(filename) +' %s' %rospy.get_time()
             
-
-
 if __name__ == "__main__":
     eh = TangoPoseCalc()
     r = rospy.Rate(5) # 5hz
@@ -186,3 +159,8 @@ if __name__ == "__main__":
         # eh.tango_get_angle_to_go()
         # Needs to get its targets chosen before this will work yet :P.
         eh.play_audio()
+        try:
+            eh.sound_publish()
+        except rospy.ROSInterruptException:
+            pass
+
