827d30c9877bacb36a9932323c6f59827738923f
pdemetci <pinardemetci@gmail.com>
2015-07-03 11:38:51 -0400

    It works! Can store multiple locations and publish the one you choose using number keys

--- eye_helper/scripts/landmark.py
+++ eye_helper/scripts/landmark.py
@@ -2,27 +2,22 @@
 
 from tango_tracker import Tango_tracker
 import rospy
-import numpy as np
 from geometry_msgs.msg import PointStamped, Point, Pose
 from sensor_msgs.msg import CameraInfo, PointCloud, CompressedImage
 import cv2
 from std_msgs.msg import Header
-from tf import TransformListener
-from threading import Lock
 
 class Landmark(object):
 	"""
-	Draft for setting up landmarks
+	Draft for setting up landmarks.
+	Publishes Tango's position to /clicked_point topic as a target in order to save current location.
 	"""
 	def __init__(self,tracker):
 		self.tracker=tracker
 		self.landmarks=[]
 		self.pub = rospy.Publisher("/clicked_point",PointStamped,queue_size=10)
-        # self.rospack = rospkg.RosPack() #May not need this?
-
 
 		#----------Publishing and subscribing to topics/nodes----------
-
 		# rospy.init_node('landmark_position')
 		# rospy.Subscriber('/landmark_position', PointStamped, self.add_landmark)
 		# self.pub = rospy.Publisher('/set_landmark', PointStamped, queue_size=10)
@@ -32,30 +27,32 @@ class Landmark(object):
 		When spacebar is down, gets Tango's position
 		"""
 
-		cv2.namedWindow("mywin")
+		cv2.namedWindow("set_landmark")
 		k = cv2.waitKey(5)
 		if ord(' ') == (k & 255):
 			self.add_landmark()
-			print self.landmarks
-			print self.landmark_number
+		for i in range(0,10):
+			i=str(i)
+			if ord(i) == (k & 255):
+				self.publish_landmark(int(i)-1)
+
 
 	def landmark_number(self):
 		"""
 		Returns the number of landmarks set
 		"""
-		return len(self.landmarks)
+		return len(self.landmarks), self.landmarks
 
 	def add_landmark(self):
 		"""
-		blah blah
+		Publishes Tango's position to the /clicked_point topic
 		"""
 		self.landmarks.append((self.tracker.x, self.tracker.y, self.tracker.z))
-
-		point_msg = PointStamped(header=Header(frame_id="depth_camera"),point=Point(y=self.tracker.y,z=self.tracker.z,x=self.tracker.x))
+		print self.landmarks
+		
+	def publish_landmark(self, i):
+		point_msg = PointStamped(header=Header(frame_id="depth_camera"),point=Point(y=self.landmarks[i][1],z=self.landmarks[i][2],x=self.landmarks[i][0]))
 		self.pub.publish(point_msg)
-
-		# point_msg = PointStamped(header=Header(stamp=msg.header.stamp,frame_id="depth_camera"),point=Point(y=tracker.y, z=tracker.z, x=tracker.x))
-		# self.landmarks.append(point_msg)
 
 
 if __name__ == "__main__":
