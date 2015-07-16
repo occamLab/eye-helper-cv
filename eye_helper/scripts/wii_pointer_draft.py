dbfa3179d79c57c13f826ee546ab7f56674558de
matthewruehle <matthew.ruehle@students.olin.edu>
2015-07-16 15:01:39 -0400

    closer to working

--- eye_helper/scripts/wii_pointer_draft.py
+++ eye_helper/scripts/wii_pointer_draft.py
@@ -81,25 +81,38 @@ class Wii_pointer():
         self.last_reading = rospy.Time.now()
         self.maybe_angle = [0,0,0]
         self.resting = [0,0,0]
-        self.target = [-700, 0, 100]
+        self.target = [-70, 0, 10]
         self.index = 0
 
 
     def run(self):
         self.poller.poll()
-        self.mote.dispatch(self.event)
+        try:
+            self.mote.dispatch(self.event)
+        except IOError:
+            # print "IOError in run dispatch"
+            pass
         if self.event.type == xwiimote.EVENT_MOTION_PLUS:
             change = self.event.get_abs(0)
             now = rospy.Time.now()
             dt = now - self.last_reading
             self.last_reading = now
             for i in range(3):
-                self.current[i] += (change[i] - self.resting[i])*(dt.nsecs/1000000000.0)*(1.45/20.0)**2 #mucking around to degrees
+                self.current[i] += (change[i] - self.resting[i])*(dt.nsecs/1000000000.0)*(1.492/20.0)**2 #mucking around to degrees
             self.index += 1
             if self.index%100 == 0:
                 print [int(i) for i in self.current], '\t \t || \t \t', change, "\t \t || \t \t", dt.nsecs/1000000000.0
                 pass
-            # self.check_if_close()
+            self.check_if_close()
+        elif self.event.type == xwiimote.EVENT_KEY:
+            (code, state) = self.event.get_key()
+            if state == 1 or True:
+                if code == 4:
+                    self.set_resting()
+                elif code == 5:
+                    self.set_zero()
+            #Keys: 0 = left, 1: right, 2: up, 3 = down, 4 = A, 5 = B, 6 = +, 7 = -, 8 = home, 9 = 1, 10 = 2.
+            print code, '\t || \t', state
 
     def check_if_close(self):
         """
@@ -108,11 +121,14 @@ class Wii_pointer():
         # distance = math.sqrt((self.target[0]-self.current[0])**2 + (self.target[1]-self.current[1])**2 + (self.target[2]-self.current[2])**2)
         distance = math.sqrt((self.target[0]-self.current[0])**2 + (self.target[2]-self.current[2])**2)
         # print distance
-        rospy.sleep(.01) # frees up the wm again. can probably be a lot less time.
-        if distance < 100:
-            self.mote.rumble(True)
-        else:
-            self.mote.rumble(False)
+        # rospy.sleep(.01) # frees up the wm again. can probably be a lot less time.
+        try:
+            if distance < 10:
+                self.mote.rumble(True)
+            else:
+                self.mote.rumble(False)
+        except IOError:
+            "ioerror in check rumble thingie."
 
 
     def set_zero(self):
@@ -122,18 +138,24 @@ class Wii_pointer():
         self.index = 0
         drifts = []
 
-        while self.index < 500: #...first handful of signals are always wacky; this gets it to ignore them.
-            self.poller.poll()
-            self.mote.dispatch(self.event)
-            self.index += 1
-
-        while self.index < 1000:
-            self.poller.poll()
-            self.mote.dispatch(self.event)
-            reading = self.event.get_abs(0)
-            if self.event.type == xwiimote.EVENT_MOTION_PLUS:
-                drifts.append(reading)
+        while self.index < 250: #...first handful of signals are always wacky; this gets it to ignore them.
+            try:
+                self.poller.poll()
+                self.mote.dispatch(self.event)
                 self.index += 1
+            except IOError:
+                print "ioerror in set_resting waiting period"
+
+        while self.index < 500:
+            try:
+                self.poller.poll()
+                self.mote.dispatch(self.event)
+                reading = self.event.get_abs(0)
+                if self.event.type == xwiimote.EVENT_MOTION_PLUS:
+                    drifts.append(reading)
+                    self.index += 1
+            except IOError:
+                print "ioerror in set_resting drift getting period"
         avg_reading = [(sum(i[j] for i in drifts)/len(drifts)) for j in range(3)]
         self.resting = avg_reading
         print "resting: \t", self.resting
@@ -146,6 +168,6 @@ if __name__ == "__main__":
     wm.set_resting()
     while not rospy.is_shutdown():
         wm.run()
-        if rospy.Time.now() - start_time > rospy.Duration(80):
+        if rospy.Time.now() - start_time > rospy.Duration(180):
             print wm.index, '\t', wm.current
             exit()
