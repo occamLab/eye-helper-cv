1403134f47e24eff0795c9844b19006ea0ee1234
pdemetci <pinardemetci@gmail.com>
2015-07-27 16:42:53 -0400

    Setting landmarks and trail following with Wii. A draft for now

--- eye_helper/scripts/landmark.py
+++ eye_helper/scripts/landmark.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import rospkg
 from geometry_msgs.msg import PointStamped, Point, Pose
 from std_msgs.msg import Int32, Int32MultiArray, Bool, Header
 from sensor_msgs.msg import Joy, JoyFeedback, CameraInfo, PointCloud, CompressedImage
@@ -22,8 +23,10 @@ class Landmark():
 
 	def __init__(self,tracker):
 		self.tracker=tracker
-		self.states=['START_UP', 'INITIALIZING_LANDMARK', 'ADDING_TRAILS', 'TOGGLING_LANDMARKS', 'PUBLISHING_TRAILS' ]
+		self.states=['START_UP', 'INITIALIZING_LANDMARK', 'ADDING_TRAILS', 'TOGGLING_LANDMARKS', 'PUBLISHING_TRAILS']
 		self.state='START_UP'
+		self.rospack = rospkg.RosPack()
+		self.filepath= self.rospack.get_path('eye_helper') + "/../GeneratedSoundFiles/Landmark_sound_files/"
 
 		#Landmarks and trails:
 		self.landmark_options=[]
@@ -31,48 +34,62 @@ class Landmark():
 		self.trail=[]
 		self.landmark_name=None
 		self.landmark_number=1
+		self.published_landmark = None
 
 		#Publishing stuff
 		self.pub = rospy.Publisher("/landmark_point",PointStamped,queue_size=10)
 		rospy.Subscriber('/wii_buttons',Int32 , self.handle_button)
 		self.data=None
+		self.threshold = 0.15 #thing to change
 		#self.last_added=rospy.Publisher("/last_landmark", PointStamped, queue_size=10)
 
 	def handle_button(self,msg):
 		self.data = msg.data
 		if msg.data ==4: 
 			self.state='INITIALIZING_LANDMARK'
-			self.initiate_landmark()
 		if msg.data == 9:
 			self.state='ADDING_TRAILS'
-			self.add_trail()
-		if msg.data == 8:
+		if msg.data == 2 or msg.data ==3:
 			self.state='TOGGLING_LANDMARKS'
-			self.landmark_menu()
-		if msg.data == 10: #fix this
+		if msg.data == 8: #fix this
 			self.state='PUBLISHING_TRAILS'
-			self.publish_trail()
 
-	# def run(self):
-	# 	if self.state == 'INITIALIZING_LANDMARK':
-
-		
-		# print code
-		# if self.msg.data == 4:
-		# 	self.add_landmark
 	def run(self):
-		pass
+		if self.state == 'START_UP':
+			pass
+
+		if self.state == 'INITIALIZING_LANDMARK':
+			self.initiate_landmark()
+			self.state = 'START_UP'
+
+		if self.state == 'ADDING_TRAILS':
+			self.add_trail()
+			self.state = 'START_UP'
+
+		if self.state == 'TOGGLING_LANDMARKS':
+			self.landmark_menu()
+			self.state = 'START_UP'
+
+		if self.state == 'PUBLISHING_TRAILS':
+			self.publish_trail()
+			self.state = 'START_UP'
+
+		if self.state not in self.states:
+			print 'State Error: self.state value is invalid'
 
 	def initiate_landmark(self):
 		self.landmark_name='landmark'+str(len(self.landmarks)+1)
 		self.landmarks[self.landmark_name]=[]
-		# self.landmark_options.append(self.landmark_name)
+		print 'Initialized landmark' + str(self.landmark_name) # need to make this into a sound file.
+		self.data=None
 
 	def add_trail(self):
 		current_position=(self.tracker.x, self.tracker.y, self.tracker.z)
 		trail_list = self.landmarks[self.landmark_name]
 	 	trail_list.append(current_position)
 	 	print self.landmarks
+	 	print 'added point on trail for landmark'+ str(self.landmark_name) # need to make this into a sound file.
+	 	self.data=None
 
 	def landmark_menu(self):
 		if self.data == 2: #up
@@ -87,6 +104,19 @@ class Landmark():
 	def publish_trail(self):
 		trails = self.landmarks['landmark'+str(self.landmark_number)]
 		print trails
+		trail_number=0
+		if len(trails) >0:
+			while trail_number>=0 or trail_number <len(trails):
+				point_msg = PointStamped(header=Header(frame_id="odom", stamp=self.tracker.pose_timestamp), point=Point(x=trails[trail_number][0],y=trails[trail_number][1],z=trails[trail_number][2]))
+				self.pub.publish(point_msg)
+				print (trails[trail_number][0],trails[trail_number][1],trails[trail_number][2])
+				if self.tracker.xy_distance<=0.15 and trail_number != len(trails)-1:
+					trail_number += 1
+				# if trail_number == len(trails)-1:
+				# 	print "You've arrived" #Turn it into a sound file
+		else:
+			print 'Error: No trail points!'
+		self.data=None
 
 	# def drop_(self):
 	# 	current_position=(self.tracker.x, self.tracker.y, self.tracker.z)
@@ -162,5 +192,5 @@ if __name__ == "__main__":
     r = rospy.Rate(5) # 5hz
     while not rospy.is_shutdown():
         tt.refresh_all()
-        lm.landmark_menu()
+        lm.run()
         r.sleep()
